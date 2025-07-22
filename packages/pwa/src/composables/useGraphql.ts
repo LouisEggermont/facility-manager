f0421073ccae1aeb0adedec3b46fe6141a233284
LouisEggermont <louis.eggermont@student.howest.be>
2025-07-22 20:31:00 +0200

    add authentication

--- packages/pwa/src/composables/useGraphql.ts
+++ packages/pwa/src/composables/useGraphql.ts
@@ -3,14 +3,29 @@ import {
   InMemoryCache,
   createHttpLink,
 } from '@apollo/client/core'
+import { setContext } from '@apollo/client/link/context'
+
+import useFirebase from './useFirebase'
+
+const { firebaseUser } = useFirebase()
 
 const httpLink = createHttpLink({
   uri: 'http://[::1]:3000/graphql', // will fix this later with environment variables
+  credentials: 'same-origin',
 })
 
+const authLink = setContext(async (_, { headers }) => ({
+  headers: {
+    ...headers,
+    authorization: (await firebaseUser.value?.getIdToken())
+      ? `Bearer ${await firebaseUser.value?.getIdToken()}`
+      : '',
+  },
+}))
+
 const apolloClient = new ApolloClient({
-  link: httpLink,
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 })
 
 export default () => {
