a3d24207ae300477683409245c54db530f9db1b6
LouisEggermont <louis.eggermont@student.howest.be>
2025-07-22 21:30:19 +0200

    fix: add TypeORM integration for Issue entity in IssuesModule

--- packages/api/src/issues/issues.module.ts
+++ packages/api/src/issues/issues.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { IssuesService } from './issues.service'
 import { IssuesResolver } from './issues.resolver'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Issue } from './entities/issue.entity'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Issue])],
   providers: [IssuesResolver, IssuesService],
 })
 export class IssuesModule {}
