4b13257c47ac53e108d6f4f1f66d1c4b4cb03ea7
LouisEggermont <louis.eggermont@student.howest.be>
2025-07-24 15:02:03 +0200

    chore: update ESLint configuration and TypeScript settings for improved linting and compatibility

--- packages/api/eslint.config.mjs
+++ packages/api/eslint.config.mjs
@@ -1,45 +1,127 @@
-// eslint.config.mjs
-import tseslint from 'typescript-eslint'
-import globals from 'globals'
+// // @ts-check
+// import eslint from '@eslint/js'
+// // import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+// import globals from 'globals'
+// import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
+// export default tseslint.config(
+//   {
+//     ignores: ['eslint.config.mjs'],
+//   },
+
+//   eslint.configs.recommended,
+//   ...tseslint.configs.recommendedTypeChecked,
+//   // eslintPluginPrettierRecommended,
+//   {
+//     languageOptions: {
+//       globals: {
+//         ...globals.node,
+//         ...globals.jest,
+//       },
+//       sourceType: 'commonjs',
+//       parserOptions: {
+//         project: 'tsconfig.json',
+//         projectService: true,
+//         tsconfigRootDir: import.meta.dirname,
+//       },
+//     },
+//   },
+//   {
+//     rules: {
+//       '@typescript-eslint/no-explicit-any': 'off',
+//       '@typescript-eslint/no-floating-promises': 'warn',
+//       '@typescript-eslint/no-unsafe-argument': 'warn',
+//     },
+//   },
+// )
+
+// const { defineConfig, globalIgnores } = require('eslint/config')
+
+// const tsParser = require('@typescript-eslint/parser')
+// const typescriptEslintEslintPlugin = require('@typescript-eslint/eslint-plugin')
+// const globals = require('globals')
+// const js = require('@eslint/js')
+
+// const { FlatCompat } = require('@eslint/eslintrc')
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+//   recommendedConfig: js.configs.recommended,
+//   allConfig: js.configs.all,
+// })
+
+// module.exports = defineConfig([
+//   {
+//     languageOptions: {
+//       parser: tsParser,
+//       sourceType: 'module',
+
+//       parserOptions: {
+//         project: 'tsconfig.json',
+//         tsconfigRootDir: __dirname,
+//       },
+
+//       globals: {
+//         ...globals.node,
+//         ...globals.jest,
+//       },
+//     },
+
+//     plugins: {
+//       '@typescript-eslint': typescriptEslintEslintPlugin,
+//     },
+
+//     extends: compat.extends('plugin:@typescript-eslint/recommended'),
+
+//     rules: {
+//       '@typescript-eslint/interface-name-prefix': 'off',
+//       '@typescript-eslint/explicit-function-return-type': 'off',
+//       '@typescript-eslint/explicit-module-boundary-types': 'off',
+//       '@typescript-eslint/no-explicit-any': 'off',
+//     },
+//   },
+//   globalIgnores(['**/.eslintrc.js']),
+// ])
+
+import eslint from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default [
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['dist/**', 'node_modules/**'],
   },
   {
+    files: ['**/*.ts'],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
-        project: './tsconfig.json',
+        project: ['./tsconfig.json'],
         tsconfigRootDir: new URL('.', import.meta.url).pathname,
-        sourceType: 'module',
       },
       globals: {
         ...globals.node,
         ...globals.jest,
       },
     },
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-    },
-  },
-  ...tseslint.configs.recommendedTypeChecked,
-  {
     rules: {
-      // adjusting rules to allow for more flexibility in the codebase
-      '@typescript-eslint/no-unsafe-return': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-
-      '@typescript-eslint/no-explicit-any': 'off',
+      // ...eslint.configs.recommended.rules,
+      ...tseslint.configs.recommendedTypeChecked[0].rules,
+      // '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
     },
   },
-)
-// // @ts-check
+]
+
+// @ts-check
 // import eslint from '@eslint/js'
 // // import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 // import globals from 'globals'
